49edff4f1bc8274586a40a9730d14ab02a1598bc
Zachary Elliott <zach@nyu.edu>
2013-01-30 19:53:45 -0500

    Adding function to the `on_styleshee_saved` hook to generate monolithic HTML files for faster deployment.

--- config.rb
+++ config.rb
@@ -22,3 +22,25 @@ relative_assets = true
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+
+require 'nokogiri'
+
+html_dir = ""
+
+sa_targets = Hash.new { |h,k| h[k] = "#{k}"[7..-1] }
+sa_targets["screen"] = "index"
+
+on_stylesheet_saved do | f |
+  name = File.basename(f).chomp(File.extname(f))
+
+  doc = Nokogiri::HTML(File.read(html_dir + sa_targets[name] + ".html"))
+  head = (doc/"head").first
+  style = doc.create_element("style")
+
+  style.inner_html = File.read(f)
+  (head/"link [rel='stylesheet']").remove
+  head << style
+
+  File.open(html_dir + sa_targets[name] + ".sa.html", "w") {|fh| fh.write(doc)}
+end
