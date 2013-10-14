21e603abfc29a9a234a5c993480970ccf46da089
Mark Reilly <mark@alienresident.net>
2013-10-14 11:45:00 -0400

    Bumped numbering

--- lib/style-tiles.rb
+++ lib/style-tiles.rb
@@ -17,8 +17,8 @@ Compass::Frameworks.register('style-tiles', :path => extension_path)
 #    a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module Style_Tiles
-  VERSION = "0.1.4"
-  DATE = "2013-08-07"
+  VERSION = "0.1.5beta"
+  DATE = "2013-10-13"
 end
 
 # This is where any custom SassScript should be placed. The functions will be  
